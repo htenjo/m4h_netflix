9c24fb37216221e666f6fa09c5dea5ba100455dc
Hernan Tenjo <zerovirus23@gmail.com>
2017-07-14 00:14:28 -0500

    home page almost complete (just pending the color gradients)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 function openMenu() {
-    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeMenu() {
     document.getElementById("mySidenav").style.width = "0";
+}
+
+function assignQR(socialIcon){
+  var icon = document.getElementById('qr-icon-src');
+  icon.src = "img/qr_" + socialIcon + ".png";
 }
