7111656b87e4051e2055f4fd3b66f06d64445342
Hernan Tenjo <zerovirus23@gmail.com>
2017-07-17 00:06:22 -0500

    slider working

--- js/script.js
+++ js/script.js
@@ -10,3 +10,38 @@ function assignQR(socialIcon){
   var icon = document.getElementById('qr-icon-src');
   icon.src = "img/qr_" + socialIcon + ".png";
 }
+
+
+$(document).ready(function(){
+  $('.slider-items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+});
+        
