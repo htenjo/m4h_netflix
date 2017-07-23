22a2648beebde70e2511dc25732533eb583ac87b
Hernan Tenjo <zerovirus23@gmail.com>
2017-07-23 18:00:30 -0500

    Final final... Done?

--- js/script.js
+++ js/script.js
@@ -15,18 +15,18 @@ function assignQR(socialIcon){
 $(document).ready(function(){
   $('.slider-items').slick({
     infinite: true,
-    centerMode: true,
+    //centerMode: true,
     slidesToShow: 4,
-    slidesToScroll: 1,
-    variableWidth: true,
+    slidesToScroll: 4,
+    //swipeToSlide: true,
+    //variableWidth: true,
     responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
             slidesToScroll: 3,
-            infinite: true,
-            dots: true
+            infinite: true
           }
         },
         {
